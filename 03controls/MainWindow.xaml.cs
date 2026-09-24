8274db27f1029fb638a5208f6ea3bf00ff165600
AdrianPalaciosTenllladoo <apalacios.dam@institutcampalans.net>
2026-09-24 16:16:00 +0200

    el boton de colores

--- 03controls/MainWindow.xaml.cs
+++ 03controls/MainWindow.xaml.cs
@@ -23,6 +23,39 @@ namespace _03controls
         public MainWindow()
         {
             InitializeComponent();
+            Grid elMeuGrid = new Grid();
+            this.Content = elMeuGrid;
+
+            Button btn = new Button();
+
+            btn.Width = 150;
+            btn.Height = 50;
+            btn.FontSize = 15;
+            
+            WrapPanel elMeuWrap = new WrapPanel();
+
+            TextBlock txt1 = new TextBlock();
+            txt1.Text = "Hola";
+            txt1.Foreground = Brushes.Red;
+            elMeuWrap.Children.Add(txt1);
+
+            TextBlock txt2 = new TextBlock();
+            txt2.Text = "Adeu";
+            txt2.Foreground = Brushes.Yellow;
+            elMeuWrap.Children.Add(txt2);
+
+            TextBlock txt3 = new TextBlock();
+            txt3.Text = "Andreu";
+            txt3.Foreground = Brushes.Green;
+            elMeuWrap.Children.Add(txt3);
+
+
+            btn.Content = elMeuWrap;
+            elMeuGrid.Children.Add(btn);
+            
+
+
         }
+        
     }
 }
